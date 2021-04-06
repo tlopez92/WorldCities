7c60c1db97693683860e31e8d3722c940c8f6c0b
Walter TimothyJos Lopez <Walter.Lopez@everbank.com>
2021-04-05 23:20:08 -0400

    Added the CountriesController.cs and CitiesController.cs controllers.

---
+++ WorldCities/Controllers/CitiesController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WorldCities.Data;
+using WorldCities.Data.Models;
+
+namespace WorldCities.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CitiesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<City>>> GetCities()
+        {
+            return await _context.Cities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<City>> GetCity(int id)
+        {
+            var city = await _context.Cities.FindAsync(id);
+            if (city is null)
+                return NotFound();
+
+            return city;
+        }
+        
+        // PUT: api/Cities/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCity(int id, City city)
+        {
+            if (id != city.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(city).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+        // POST: api/Cities
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<City>> PostCity(City city)
+        {
+            _context.Cities.Add(city);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetCity", new { id = city.Id }, 
+                city);
+        }
+        // DELETE: api/Cities/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<City>> DeleteCity(int id)
+        {
+            var city = await _context.Cities.FindAsync(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            _context.Cities.Remove(city);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        private bool CityExists(int id)
+        {
+            return _context.Cities.Any(e => e.Id == id);
+        }
+    }
+}
